13a9541378681f758f0293279d409b133f86f2ae
CekoKaxi <cekokaxi371@gmail.com>
2024-06-05 20:03:21 -0300

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,10 +1,7 @@
 // Lista de domínios para verificar
 const domains = [
     'https://www.socialadirecionar.online',
-    'https://www.friendshipquotes.site',
-    'https://www.hg0088.site',
-    'https://www.ivirtual.site',
-    'https://reverselookup.site'
+    'https://www.socialadirecionar.online'
 ];
 
 // Função para verificar a disponibilidade do domínio
